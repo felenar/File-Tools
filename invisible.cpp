dae4dc9e8046c65ddb52ae37dad33a7edaf05fb3
DACOOLISTOFDOODS <106041210+DACOOLISTOFDOODS@users.noreply.github.com>
2022-05-31 16:22:34 -0400

    putting things in slots

--- invisible.cpp
+++ invisible.cpp
@@ -1,11 +1,14 @@
 //compile with -mwindows
-
+/*arguments should be "1" "cut  " or "copy " "C:\...\test\" */
+#include <string>
 #include <fstream>
 
-int main() {
+int main(int argc, char **argv) {
+    if(argc != 4) return 0;
     std::ofstream file;
-    file.open("test.txt");
-    file << "test";
+    std::string filename = "slot" + std::string(argv[1]) + ".txt";
+    file.open(filename);
+    file << argv[2] + std::string(argv[3]);
     file.close();
     return 0;
 }
