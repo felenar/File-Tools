a5d5d38383096e3e40ee31453772939561d61912
DACOOLISTOFDOODS <106041210+DACOOLISTOFDOODS@users.noreply.github.com>
2022-07-07 22:35:52 -0400

    add to path

--- invisible.cpp
+++ invisible.cpp
@@ -4,6 +4,8 @@ or "paste" "1 or 2" "C:\...\test" */
 #include <string>
 #include <fstream>
 #include <windows.h>
+#include <stringapiset.h>
+
 
 int main(int argc, char **argv) {
     std::string check = argv[1];
@@ -33,32 +35,8 @@ int main(int argc, char **argv) {
         //ShellExecute(0, "runas", "cmd.exe", "/k " + command.c_str(), 0, SW_SHOWNORMAL);
         // HINSTANCE result = ShellExecute(0, "runas", "notepad.exe", 0, 0, SW_SHOWNORMAL);
     } if(check == "path") {
-        // const std::string envPath = argv[2];
-        // HKEY key;
-        // TCHAR value[255];
-        // DWORD value_length = 255;
-
-        // DWORD dwActualType;
-
-        // RegOpenKey(HKEY_LOCAL_MACHINE, "SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment",&key);
-        // RegQueryValueEx(key, "Path", NULL, &dwActualType, (LPBYTE)&value, &value_length);
-        // RegCloseKey(key);
-
-        // std::string pathS = std::string(value) + ";" + std::string(argv[2]);
-        // TCHAR *path=new TCHAR[pathS.size()+1];
-        // path[pathS.size()]=0;
-        // std::copy(pathS.begin(),pathS.end(),path);
-
-        // RegOpenKey(HKEY_LOCAL_MACHINE, "SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment",&key);
-        // RegSetValueEx(key, "Path", 0, REG_EXPAND_SZ, (LPBYTE)path, strlen(value)*sizeof(char));
-        // RegCloseKey(key);
-
-        // std::ofstream file (".txt");
-        // file << std::string(pathS);
-        // file.close();
-
-
-        std::string cmd = "setx PATH \"%PATH%;";
-        system((cmd + argv[2] + "\" /M").c_str());
+        std::string cmd = "%windir%\\System32\\setx.exe PATH \"%PATH%;";
+        cmd = cmd + argv[2] + "\"";
+        system((cmd).c_str());
     } return 0;
 }
